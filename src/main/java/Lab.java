493a156dc2f1e5d4e83a517dfbb08111ce43824c
Ronny Barahona <ronnybarahona10@gmail.com>
2023-02-23 22:11:46 +0000

    now

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,20 +1,10 @@
-
-/**
- * In order to make our programs even more impressive and interactive, we need to learn how to better leverage
- * boolean expressions. This will allow our programs to follow their own logic. For this example, you should look into
- * the different operators for basic boolean expressions.
- * 
- * More resources: https://www.freejavaguide.com/boolean_operators.htm
- * 
- * NOTE: All of these problems can be solved with one line with boolean operators - eg, 'return !b;'
- */
 public class Lab {
     /**
      * @param b a true/false value.
      * @return if b is true, return true. if b is false, return false.
      */
     public boolean returnBoolean(boolean b){
-        return false;
+        return b;
     }
 
     /**
@@ -23,7 +13,7 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnNot(boolean b){
-        return false;
+        return !b;
     }
 
     /**
@@ -33,7 +23,7 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnAnd(boolean b1, boolean b2){
-        return false;
+        return b1 && b2;
     }
 
     /**
@@ -43,8 +33,6 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnOr(boolean b1, boolean b2){
-        return false;
+        return b1 || b2;
     }
-
-
 }
